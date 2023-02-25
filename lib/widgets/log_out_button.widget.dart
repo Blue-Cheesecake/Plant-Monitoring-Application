65a9716f3c6b4cc2957420553a10a7e437bd7181
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-02-25 13:42:46 +0700

    feat: logout dialog

--- lib/widgets/log_out_button.widget.dart
+++ lib/widgets/log_out_button.widget.dart
@@ -7,13 +7,49 @@ class LogOutButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        Navigator.of(context).pushReplacementNamed(HomeView.routeName);
-      },
+      onPressed: () => _showWarningDialog(context),
       icon: Image.asset(
         "assets/icons/logout.png",
         width: 25,
       ),
+    );
+  }
+
+  Future<void> _showWarningDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text(
+            "Warning",
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          content: const Text("Are you sure you're logging out?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  // TODO: Add Firebase Signout Here
+
+                  Navigator.of(context)
+                      .pushReplacementNamed(HomeView.routeName);
+                },
+                child: const Text("Yes")),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                "Cancel",
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
